2dd8d51f2f3010a9144ed92574b8a0544cc36207
Gregory Nutt <gnutt@nuttx.org>
2019-10-02 14:56:20 -0600

    /buildtest/doarm.sh:  Another toolchain update.

--- buildtest/doarm.sh
+++ buildtest/doarm.sh
@@ -11,7 +11,8 @@ fi
 
 rm -f armlist.dat
 if [ "X${HOSTOS}" == "XCygwin" ]; then
-  TOOLCHAIN_BIN="/cygdrive/c/Program Files (x86)/GNU Tools ARM Embedded/4.9 2015q2/bin"
+#  TOOLCHAIN_BIN="/cygdrive/c/Program Files (x86)/GNU Tools ARM Embedded/4.9 2015q2/bin"
+  TOOLCHAIN_BIN="/cygdrive/c/Program Files (x86)/GNU Tools ARM Embedded/8 2019-q3-update/bin"
   cat armlist.template | sed -e "s/_EABIx/_EABIW/g" >armlist.dat
   OPTIONS="-w -c"
 else
