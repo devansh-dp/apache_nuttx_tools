24521958c117c2e822b2631a36047f8d9138689e
Gregory Nutt <gnutt@nuttx.org>
2019-11-06 07:57:45 -0600

    builtest/doarm.sh:  Allow for a '-j <ncpus>' command line option.

--- buildtest/doarm.sh
+++ buildtest/doarm.sh
@@ -2,8 +2,16 @@
 #set -x
 
 WD=$PWD
-TESTLIST=$1
 HOSTOS=$(uname -o 2>/dev/null || echo "Other")
+
+unset CPUOPT
+if [ "X$1" == "X-j" ]; then
+  shift
+  CPUOPT="-j $1"
+  shift
+fi
+
+TESTLIST=$1
 
 if [ -z "${PATH_ORIG}" ]; then
   export PATH_ORIG="${PATH}"
@@ -20,8 +28,7 @@ else
   cat armlist.template | sed -e "s/_EABIx/_EABIL/g" >armlist.dat
   ENVOPT=-l
 fi
-#CPUOPT="-j 4"
-unset CPUOPT
+
 export PATH="${TOOLCHAIN_BIN}:/sbin:/usr/sbin:${PATH_ORIG}"
 
 if [ -z "$TESTLIST" ]; then
