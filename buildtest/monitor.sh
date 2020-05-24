8febcdbe97f1adeec082e38c12c0b20fe776ae0e
Gregory Nutt <gnutt@nuttx.org>
2020-05-24 14:31:29 -0600

    buildroot/monitor.sh:  Some tweaks to reduce the amount of output.

--- buildtest/monitor.sh
+++ buildtest/monitor.sh
@@ -8,8 +8,14 @@ fi
 tmp=`cat $FILE | grep -v "^#" | wc -`
 ntotal=`echo $tmp | cut -d' ' -f 1`
 
+rm -f err1.txt err2.txt
+touch err1.txt
+
 while [ true ]; do
-  grep -in error armtest.log
+  grep -n error: armtest.log | grep -v "[#]warning" >err2.txt
+  diff err1.txt err2.txt | sed -e "s/^[>] [0-9]*://g"
+  mv err2.txt err1.txt
+
   wc -l armtest.log
   tmp=`grep Configuring armtest.log | wc -`
   nsofar=`echo $tmp | cut -d' ' -f 1`
